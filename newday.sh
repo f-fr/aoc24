05fd403294f04915df0d2e6e3825ba8561f216b4
Frank Fischer <frank-fischer@shadow-soft.de>
2024-11-21 08:21:05 +0100

    newday.sh: create empty testcase files

--- newday.sh
+++ newday.sh
@@ -15,4 +15,8 @@ hg add $src
 mkdir -p "input/$day"
 curl "https://adventofcode.com/2023/day/$1/input" --compressed -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0' -H "Cookie: session=$SESSIONCOOKIE" > input/$day/input1.txt
 
+# create empty testcases
+[ -f input/$day/test_part1.txt ] || echo "EXPECTED: 42" > input/$day/test_part1.txt
+[ -f input/$day/test_part2.txt ] || echo "EXPECTED: 42" > input/$day/test_part2.txt
+
 popd >& /dev/null

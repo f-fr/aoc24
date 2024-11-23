a59368c7f7546939add27f3fa0994f77d4a9b55c
Frank Fischer <frank-fischer@shadow-soft.de>
2024-11-23 17:17:27 +0100

    newday.sh: update year to 2024

--- newday.sh
+++ newday.sh
@@ -19,7 +19,7 @@ if [ "$day" != "00" ]; then
 
     # download input
     mkdir -p "input/$day"
-    curl "https://adventofcode.com/2023/day/$1/input" --compressed -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0' -H "Cookie: session=$SESSIONCOOKIE" > input/$day/input1.txt
+    curl "https://adventofcode.com/2024/day/$1/input" --compressed -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0' -H "Cookie: session=$SESSIONCOOKIE" > input/$day/input1.txt
 fi
 
 # Auto generate files for all known days

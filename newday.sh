301b47ac2f6cbd4a4495612babd48581f0f8c09e
Frank Fischer <frank-fischer@shadow-soft.de>
2024-11-23 16:04:09 +0100

    newday.sh: without argument just updays days.inc and *.lpi files

--- newday.sh
+++ newday.sh
@@ -7,38 +7,48 @@ src="src/day${day}.pas"
 
 SESSIONCOOKIE=$(cat .session)
 
-# all known days
-DAYS=$(ls src/day??.pas | sed -e 's!src/day\(.*\)\.pas!\1!')
+if [ "$day" != "00" ]; then
+    # new unit from template
+    cp -i "src/dayTPL.pas" $src
+    sed -i -e "s/DAY/$day/" $src
+    hg add $src
 
-cp -i "src/dayTPL.pas" $src
-sed -i -e "s/DAY/$day/" $src
-printf ", Day%02d\n" $DAYS > src/days.inc
-hg add $src
+    # create empty testcases
+    [ -f input/$day/test_part1.txt ] || echo "EXPECTED: 42" > input/$day/test_part1.txt
+    [ -f input/$day/test_part2.txt ] || echo "EXPECTED: 42" > input/$day/test_part2.txt
 
-mkdir -p "input/$day"
-curl "https://adventofcode.com/2023/day/$1/input" --compressed -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0' -H "Cookie: session=$SESSIONCOOKIE" > input/$day/input1.txt
+    # download input
+    mkdir -p "input/$day"
+    curl "https://adventofcode.com/2023/day/$1/input" --compressed -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0' -H "Cookie: session=$SESSIONCOOKIE" > input/$day/input1.txt
+fi
+
+# Auto generate files for all known days
+
+DAYS=$(find src -maxdepth 1 -name "day??.pas" | sed -e 's!src/day\(.*\)\.pas!\1!')
+
+if  [ ! -z "$DAYS" ]; then
+    printf ", Day%02d\n" $DAYS > src/days.inc
+else
+    echo > src/days.inc
+fi
 
 # edit lpi to add day??.pas source files
 for lpi in aoc24*.lpi; do
     # first delete all existing src/day??.pas files as sources
     xml ed --inplace --delete '//ProjectOptions/Units/Unit[Filename[starts-with(@Value, "src/day") and contains(@Value, "pas")]]' "$lpi"
     # add all known days again
-    for day in $DAYS; do
+    for d in $DAYS; do
         xml ed --inplace \
             --subnode '//ProjectOptions/Units' -t elem --name "Unit" \
             --var newunit '$prev' \
             --subnode '$newunit' --type elem --name "Filename" \
-            --insert '$prev' --type attr --name "Value" --value $(printf "src/day%02d.pas" $day) \
+            --insert '$prev' --type attr --name "Value" --value $(printf "src/day%02d.pas" $d) \
             --subnode '$newunit' --type elem --name "IsPartOfProject" \
             --insert '$prev' --type attr --name "Value" --value "True" \
             --subnode '$newunit' --type elem --name "UnitName" \
-            --insert '$prev' --type attr --name "Value" --value $(printf "Day%02d" $day) \
+            --insert '$prev' --type attr --name "Value" --value $(printf "Day%02d" $d) \
             "$lpi"
     done
 done
-
-# create empty testcases
-[ -f input/$day/test_part1.txt ] || echo "EXPECTED: 42" > input/$day/test_part1.txt
-[ -f input/$day/test_part2.txt ] || echo "EXPECTED: 42" > input/$day/test_part2.txt
 
 popd >& /dev/null

d651ef019243e0abfbde7510068744417ec172bd
Frank Fischer <frank-fischer@shadow-soft.de>
2024-12-04 11:10:28 +0100

    04: add version using TStrings instead of TGrid

--- src/day04.pas
+++ src/day04.pas
@@ -24,7 +24,7 @@ interface
 
 implementation
 
-uses AOC, Classes;
+uses AOC, Classes, SysUtils;
 
 function Run(grid: TGrid): TResult;
 var
@@ -58,8 +58,43 @@ begin
    end
 end;
 
+function Run2(grid: TStrings): TResult;
+var
+   i,j: Integer;
+   dx, dy: Integer;
+begin
+   result[1] := 0;
+   result[2] := 0;
+
+   for i := 0 to grid.Count - 1 do grid[i] := Format('.%s.', [grid[i]]);
+   grid.Append(StringOfChar('.', grid[0].Length));
+   grid.Insert(0, grid[grid.Count - 1]);
+
+   for i := 1 to grid.Count-2 do begin
+      for j := 2 to grid[i].Length-1 do begin
+         for dx := -1 to 1 do begin
+            for dy := -1 to 1 do begin
+               if (grid[i][j] = 'X') and
+                     (grid[i+dx][j+dy] = 'M') and
+                     (grid[i+2*dx][j+2*dy] = 'A') and
+                     (grid[i+3*dx][j+3*dy] = 'S')
+               then Inc(result[1]);
+            end
+         end;
+
+         if (grid[i][j] = 'A') and
+               (((grid[i-1][j-1] = 'M') and (grid[i+1][j+1] = 'S')) or
+                ((grid[i-1][j-1] = 'S') and (grid[i+1][j+1] = 'M'))) and
+               (((grid[i-1][j+1] = 'M') and (grid[i+1][j-1] = 'S')) or
+                ((grid[i-1][j+1] = 'S') and (grid[i+1][j-1] = 'M')))
+         then Inc(result[2]);
+      end
+   end
+end;
+
 initialization
 
    RegisterDay(04, @Run, 1);
+   RegisterDay(04, @Run2, 2);
 
 end.
